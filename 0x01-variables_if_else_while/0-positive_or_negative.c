d3b14ce5d32b544040945b2c6e78d583b97f7e45
Retephytech <99274366+Retephytech@users.noreply.github.com>
2022-03-11 21:15:51 +0100

    negative and positive C programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - Entry point
  *
- * Return Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -16,7 +16,7 @@ int main(void)
 
 	if (n == 0)
 		printf("%d is zero\n", n);
-	else if ( n < 0)
+	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is positive\n", n);
