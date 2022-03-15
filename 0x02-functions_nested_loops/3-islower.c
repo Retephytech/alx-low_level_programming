32a98059f3d2ebcfed2bc7899144ac0b34749b0e
Phytech Retep <peteratteh22@gmail.com>
2022-03-15 22:51:26 +0100

    checks for lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,19 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * _islower - Functions that checks for lowercase
- * @c: character to be verified
+ *
  * Return: 1 for lowercase otherwise 0
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
+	putchar('\n');
 }
