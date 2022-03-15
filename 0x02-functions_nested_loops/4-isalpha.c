756a8d324564a1889f7606b5a03605c9679a81c8
Phytech Retep <peteratteh22@gmail.com>
2022-03-15 23:10:12 +0100

    check for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - show 1 if c otherwise show 0
+ *
+ * Return: 1 for letters otherwise return 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	putchar('\n');
+}
