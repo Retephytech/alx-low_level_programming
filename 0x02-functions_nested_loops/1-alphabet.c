ac0dabcf1609955de9a389759be872d8094800ab
Phytech Retep <peteratteh22@gmail.com>
2022-03-15 21:49:30 +0100

    print alhabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Print alphabet in lowercase
+ * 
+ * Return: is void 'using prototype'
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 97; a < 123; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+}
