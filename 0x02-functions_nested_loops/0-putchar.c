849ff747f6bc683440f0445bbe7913e40c5a8e56
Phytech Retep <peteratteh22@gmail.com>
2022-03-15 21:30:53 +0100

    program writes _putchar as output

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,9 +5,16 @@
  *
  * Return: Always 0 
  */
-char putchar(char _putchar)
+int main(void)
 {
-	putchar('_putchar');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
 	putchar('\n');
 
 	return 0;
