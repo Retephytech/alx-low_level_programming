36bc1dac46adda1cf8391c2a50cfd984c8a2338e
Phytech Retep <peteratteh22@gmail.com>
2022-03-15 20:48:25 +0100

    program writes _putchar as output

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>, "_putchar"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 
+ */
+int main(void)
+{
+	_putchar("_putchar");
+	_putchar('\n');
+
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+C Programming Language- Functions, nested loops
