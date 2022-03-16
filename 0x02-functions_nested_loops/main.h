6d7976e396b7ed06beb5b029bb10d91c3cf7c27a
Phytech Retep <peteratteh22@gmail.com>
2022-03-16 04:28:09 +0100

    editted main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,11 @@
 /**
  * Prototype of all functions used in task
  */
+#ifndef mani.h
+#define main.h
 
+int _putchar(char c);
+int putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,3 +17,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+
+#endif
