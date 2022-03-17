844c5d6a261b442d8023aa05ab08bc5ec9c4d948
Phytech Retep <peteratteh22@gmail.com>
2022-03-17 14:35:33 +0100

    print numbers except 2 & 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers except 2 & 4
+ * print_most_numbers - prints numbers except 2 & 4
  */
 void print_most_numbers(void)
 {
-	char c;
+	int c;
 
 	for (c = '0'; c <= '9'; c++)
+	{
 		if (c != '2' && c != '4')
 			_putchar(c);
+	}
 	_putchar('\n');
 }
