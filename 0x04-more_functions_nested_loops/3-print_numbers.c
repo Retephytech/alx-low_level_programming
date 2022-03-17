a130d9dcace674ef7496e39f97e0f7a9bc207881
Phytech Retep <peteratteh22@gmail.com>
2022-03-17 14:57:53 +0100

    print numbers from 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,8 +7,10 @@ void print_numbers(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
-		_putchar((num % 10) + '0');
+	for (num = '0'; num <= '9'; num++)
+	{
+		_putchar(num);
+	}
 
 	_putchar('\n');
 }
