5c7f5e261a4842c8bec7e9c08a439ca15904b3fc
Phytech Retep <peteratteh22@gmail.com>
2022-03-22 01:44:47 +0100

    using pointer int for the first time

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_t0_98 - Takes a pointer to an int and updates to 98
+ * reset_to_98 - Takes a pointer to an int and updates to 98
  * @n: The pointer
  */
 void reset_to_98(int *n)
