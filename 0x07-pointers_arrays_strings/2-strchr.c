353b565e60ab5a3e44ef720b3ba2695ee5f0c5b5
Phytech Retep <peteratteh22@gmail.com>
2022-03-28 23:09:17 +0100

    locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stddef.h>
 /**
- * _strchr - locates a character in a string
+ * _strchr - Locates a character in a string
  * @s: the string
  * @c: the character
  *
- * Return: a pointer to a character aka the character found
+ * Return: pointer to a character found
  */
 char *_strchr(char *s, char c)
 {
