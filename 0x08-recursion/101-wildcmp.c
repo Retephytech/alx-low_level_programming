b965018f6ed2aeb99f25c0185fa5347e2c7a734d
Phytech Retep <peteratteh22@gmail.com>
2022-03-31 01:18:33 +0100

    Strings can be considered identical

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,12 +13,12 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '\0')
-		return (0);
+		return (1);
 	if (*s2 == '*')
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	if (*s1 == '\0')
 	{
-		if (*s2 != *s1)
+		if (*s2 != '*')
 			return (0);
 		else
 			return (wildcmp(s1, s2 + 1));
