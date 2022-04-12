7c3e04eee05dbeac592d78956d9d8528f2907d1a
Phytech Retep <peteratteh22@gmail.com>
2022-04-12 03:26:29 +0100

    macro ABS(x) computes absolute of x

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,9 +3,10 @@
 
 /*
  * File: 3-function_like_macro.h
- * Desc: Header file that defines a macro ABS(x) that computes the absolute value x.
+ * Auth: Atteh
+ * Desc: Header file that defines a function-like macro ABS(x)
+ *       that computes the absolute value of a number x.
  */
 
 #define ABS(x) ((x) < 0 ? (-x) : (x))
-
 #endif
