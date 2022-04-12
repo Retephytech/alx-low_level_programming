60e29ddb59cf4ae4519723d10b76884bf2197fd5
Phytech Retep <peteratteh22@gmail.com>
2022-04-12 03:45:12 +0100

    initialize variable type struct dog created in dog.h

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,10 +8,12 @@
  * @age: age of the dog created.
  * @owner: owner of created dog.
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
