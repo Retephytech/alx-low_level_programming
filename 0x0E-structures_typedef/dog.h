2814ba4fc1ce1b7c95fa2888e750b331a5fffea7
Phytech Retep <peteratteh22@gmail.com>
2022-04-12 03:54:45 +0100

    structures & typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,16 +6,19 @@
  * @age: age of dog
  * @owner: owner of dog.
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
 /**
  * dog_t - Typedef for struct dog
  */
+
 typedef struct dog dog_t;
+
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
